0dcc5451e012126cf42750250cb885dc43df97cd
sumi-0011 <selina2000@naver.com>
2023-05-25 13:03:39 +0900

    컬러 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,27 +18,27 @@ module.exports = {
         /**
          * @description selection(드래그 블록)에 사용되는 컬러입니다.
          */
-        PRIMARY_LIGHT: "#c3fae8",
+        PRIMARY_LIGHT: "#F7CAC9",
 
         /**
          * @description 자기소개의 이름 부분, code tag의 darkmode에 사용되는 컬러입니다.
          */
-        PRIMARY: "#12b886",
+        PRIMARY: "#92a8d1",
 
         /**
          * @description code tag, link hover icon에 사용되는 컬러입니다.
          */
-        PRIMARY_HEAVY: "#087f5b",
+        PRIMARY_HEAVY: "#6495ed",
 
         /**
          * @description 페이지 최상단 gradient의 시작 컬러입니다.
          */
-        GRADIENT_FROM: "#51cf66",
+        GRADIENT_FROM: "#F7CAC9",
 
         /**
          * @description 페이지 최상단 gradient의 종료 컬러입니다.
          */
-        GRADIENT_TO: "#0c8599",
+        GRADIENT_TO: "#6080bc",
       },
     },
   },
